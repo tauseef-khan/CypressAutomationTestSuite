c2bb448bd830d0c1329a267ab549e45aea8e3832
tauseef-khan <tdakhan@hotmail.co.uk>
2024-09-13 22:28:18 +0100

    completed "purchasing a family box" workflow test

--- cypress/e2e/PurchaseWorkflow.cy.js
+++ cypress/e2e/PurchaseWorkflow.cy.js
@@ -1,6 +1,6 @@
 describe('Purchasing donut(s)', () => {
   
-  it('Purchasing a single donut', () => {
+  it('Purchasing a family box', () => {
     cy.visit('/')
     
     const familyBoxLink = 'body > div > div:nth-child(1) > div > div:nth-child(1) > a > div > div > div:nth-child(1)'
@@ -13,9 +13,28 @@ describe('Purchasing donut(s)', () => {
     const cartButton = 'body > astro-island > header > a:nth-child(2) > div'
     cy.get(cartButton).click()
 
-    // const shoppingCartList = 'body > div > astro-island > div:nth-child(1) > div:nth-child(2)'
-    // cy.get(shoppingCartList).within(() => {
-    //   cy.get('div').should('have.length', 2)
-    // })
+    const checkoutButton = 'body > div > astro-island > div > div > a'
+    cy.get(checkoutButton).click()
+
+    const firstNameTextField = 'body > div > astro-island > div > div > form > input:nth-child(1)'
+    cy.get(firstNameTextField).type('John') 
+
+    const lastNameTextField = 'body > div > astro-island > div > div > form > input:nth-child(2)'
+    cy.get(lastNameTextField).type('Doe')
+
+    const emailTextField = 'body > div > astro-island > div > div > form > input:nth-child(3)'
+    cy.get(emailTextField).type('johndoe@dinosdonuts.com')
+
+    const streetTextField = 'body > div > astro-island > div > div > form > input:nth-child(4)'
+    cy.get(streetTextField).type('123 Fairy Street')
+
+    const cityTextField = 'body > div > astro-island > div > div > form > input:nth-child(5)'
+    cy.get(cityTextField).type('London')
+
+    const postcodeTextField = 'body > div > astro-island > div > div > form > input:nth-child(6)'
+    cy.get(postcodeTextField).type('E1 4NS')
+
+    const payNowButton = 'body > div > astro-island > div > div > button'
+    cy.get(payNowButton).should('contain.text', "Pay Now")
   })
 })
