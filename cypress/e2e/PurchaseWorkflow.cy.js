bef4a5d6e22ca8d500a3f2af2f5b0e4716f46f45
tauseef-khan <tdakhan@hotmail.co.uk>
2024-09-14 06:08:00 +0100

    - completed "Purchase single donut" workflow - added assert in "family box" workflow for text check on the cart screen

--- cypress/e2e/PurchaseWorkflow.cy.js
+++ cypress/e2e/PurchaseWorkflow.cy.js
@@ -1,5 +1,46 @@
 describe('Purchasing donut(s)', () => {
   
+  it('Purchasing a single donut', () => {
+    cy.visit('/')
+    
+    const chocolateDreamDonutLink = 'body > div > div:nth-child(1) > div > div:nth-child(3) > a > div'
+    cy.get(chocolateDreamDonutLink).should('contains.text', "Chocolate Dream")
+    cy.get(chocolateDreamDonutLink).click()
+
+    const addToCartButton = 'body > div > astro-island > div > button'
+    cy.get(addToCartButton).click()
+
+    const cartButton = 'body > astro-island > header > a:nth-child(2) > div'
+    cy.get(cartButton).click()
+
+    const shoppingCartListSection = 'body > div > astro-island > div > div'
+    cy.get(shoppingCartListSection).should('contain.text', "Chocolate Dream")
+
+    const checkoutButton = 'body > div > astro-island > div > div > a'
+    cy.get(checkoutButton).click()
+
+    const firstNameTextField = 'body > div > astro-island > div > div > form > input:nth-child(1)'
+    cy.get(firstNameTextField).type('John') 
+
+    const lastNameTextField = 'body > div > astro-island > div > div > form > input:nth-child(2)'
+    cy.get(lastNameTextField).type('Doe')
+
+    const emailTextField = 'body > div > astro-island > div > div > form > input:nth-child(3)'
+    cy.get(emailTextField).type('johndoe@dinosdonuts.com')
+
+    const streetTextField = 'body > div > astro-island > div > div > form > input:nth-child(4)'
+    cy.get(streetTextField).type('123 Fairy Street')
+
+    const cityTextField = 'body > div > astro-island > div > div > form > input:nth-child(5)'
+    cy.get(cityTextField).type('London')
+
+    const postcodeTextField = 'body > div > astro-island > div > div > form > input:nth-child(6)'
+    cy.get(postcodeTextField).type('E1 4NS')
+
+    const payNowButton = 'body > div > astro-island > div > div > button'
+    cy.get(payNowButton).should('contain.text', "Pay Now")
+  })
+
   it('Purchasing a family box', () => {
     cy.visit('/')
     
@@ -12,6 +53,9 @@ describe('Purchasing donut(s)', () => {
 
     const cartButton = 'body > astro-island > header > a:nth-child(2) > div'
     cy.get(cartButton).click()
+
+    const shoppingCartListSection = 'body > div > astro-island > div > div'
+    cy.get(shoppingCartListSection).should('contain.text', "Triple Trouble Family Box")
 
     const checkoutButton = 'body > div > astro-island > div > div > a'
     cy.get(checkoutButton).click()
