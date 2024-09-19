eae427310fdd1d5d73174e1e659ba12e2f67b8da
tauseef-khan <tdakhan@hotmail.co.uk>
2024-09-19 08:29:43 +0100

    assert image sources on family box page

--- cypress/e2e/TripleTroubleFamilyBoxPage.cy.js
+++ cypress/e2e/TripleTroubleFamilyBoxPage.cy.js
@@ -18,5 +18,11 @@ describe('Triple Truble Family Box page', () => {
 
     const totalPriceText = 'body > div > astro-island > div:nth-child(2) > div > p:nth-child(2)'
     cy.get(totalPriceText).should('contain.text', "$19.00")
+
+    const mainImage = 'body > div > astro-island > div > figure > picture > img'
+    cy.get(mainImage).should('have.attr', 'src', 'https://media.crystallize.com/dounot/22/12/2/27/triple_trouble_famimly_box.png')
+
+    const bakeryCounterImage = 'body > div > astro-island > div > div:nth-child(2) > div > figure > picture > img'
+    cy.get(bakeryCounterImage).should('have.attr', 'src', 'https://media.crystallize.com/dounot/22/12/2/4/@100/bakery-counter.jpeg')
   })
 })
