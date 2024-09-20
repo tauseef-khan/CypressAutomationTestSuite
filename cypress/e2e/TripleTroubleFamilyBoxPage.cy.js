c9beb3b5da11b3e0d17815979a818fb307138100
tauseef-khan <tdakhan@hotmail.co.uk>
2024-09-20 21:05:35 +0100

    assert family box nutritional information

--- cypress/e2e/TripleTroubleFamilyBoxPage.cy.js
+++ cypress/e2e/TripleTroubleFamilyBoxPage.cy.js
@@ -24,5 +24,32 @@ describe('Triple Truble Family Box page', () => {
 
     const bakeryCounterImage = 'body > div > astro-island > div > div:nth-child(2) > div > figure > picture > img'
     cy.get(bakeryCounterImage).should('have.attr', 'src', 'https://media.crystallize.com/dounot/22/12/2/4/@100/bakery-counter.jpeg')
+
+    const nutritionalInfoTable = 'body > div > astro-island > div > div:nth-child(4) > div:nth-child(2)'
+    
+    cy.get(nutritionalInfoTable).within(() => {
+      cy.get('div').should('have.length', 7)
+
+      cy.get('div:nth-child(1) > p:nth-child(1)').should('contain.text', "Calories")
+      cy.get('div:nth-child(1) > p:nth-child(2)').should('contain.text', "143 kj")
+      
+      cy.get('div:nth-child(2) > p:nth-child(1)').should('contain.text', "Total fat")
+      cy.get('div:nth-child(2) > p:nth-child(2)').should('contain.text', "5.4 g")
+
+      cy.get('div:nth-child(3) > p:nth-child(1)').should('contain.text', "Cholesterol")
+      cy.get('div:nth-child(3) > p:nth-child(2)').should('contain.text', "0 mg")
+
+      cy.get('div:nth-child(4) > p:nth-child(1)').should('contain.text', "Sodium")
+      cy.get('div:nth-child(4) > p:nth-child(2)').should('contain.text', "86 mg")
+
+      cy.get('div:nth-child(5) > p:nth-child(1)').should('contain.text', "Potassium")
+      cy.get('div:nth-child(5) > p:nth-child(2)').should('contain.text', "109 mg")
+
+      cy.get('div:nth-child(6) > p:nth-child(1)').should('contain.text', "Total carbohydrates")
+      cy.get('div:nth-child(6) > p:nth-child(2)').should('contain.text', "22.8 mg")
+
+      cy.get('div:nth-child(7) > p:nth-child(1)').should('contain.text', "Protein")
+      cy.get('div:nth-child(7) > p:nth-child(2)').should('contain.text', "2.4 g")
+    })
   })
 })
